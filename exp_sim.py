f870123768941f8bef002ea01f4aaac832a1f85a
Kevin Fei <kevinfei@college.harvard.edu>
2016-06-22 19:07:29 -0400

    Deterministic simulation working

--- exp_sim.py
+++ exp_sim.py
@@ -34,12 +34,13 @@ n = 6000000 #Photons to generate
 xeps = 0.003 #X epsilon
 eps = 0.00001 #other epsilon
 
-simulation = "3200"#"200" #Do the simulation or read a file
-autocorr = False #Do the autocorrelation, kinda slow
-poincare = False
+simulation = "deterministica"#"3200"#"200" #Do the simulation or read a file
+deterministic = True
+autocorr = False
+poincare = True
 variance = False
 
-if simulation == "simulation":
+if simulation == "photons":
 	foutpop = open('pop.out','w')
 	foutx = open('xs.out','w') #x-simplified
 
@@ -125,7 +126,55 @@ if simulation == "simulation":
 
 	foutpop.close()
 	foutx.close()
-else:
+elif simulation == "deterministic":
+	T = 5 #MUST BE FLOAT, lenght of sim time
+	transtime = 0.1 #drop transients, so time to start counting data
+	samptime = 0.00001
+	sampspersec = 1 / samptime #number of samples to collect
+
+	t = 0 #Current time
+	#Random (ridiculous) conditions, so it can settle into normal range after a while.
+	x1 = 10 * random.random()
+	x2 = 10 * random.random()
+	lastt = 0 #Last time since a photon was detected
+
+	x1hist = [.7834] * int(Td / dt)
+	x2hist = [0] * int(Td / dt)
+
+	timestart = time.clock()
+
+	fout = open("detint.out","w")
+	foutx = open("detxs.out","w")
+	fout.write("{:6f}\n".format(T))
+	fout.write("{}\n".format(sampspersec))
+
+	intensities = []
+	timegraph = []
+	poincaretimes = []
+	while t < T:
+		x1 += (- 1 / T1 * x1 + beta * np.sin(x1hist[0] - x2hist[0] + phi) ** 2) * dt
+		x2 += (- 1 / T2 * x2 + beta * np.sin(x1hist[0] - x2hist[0] + phi) ** 2) * dt
+
+		if t > transtime and int(t / dt) % int(samptime / dt) == 0:
+			intensities.append(np.sin(x1hist[0] - x2hist[0] + phi) ** 2)
+			timegraph.append(t - transtime)
+			fout.write("{:6f}\n".format(np.sin(x1hist[0] - x2hist[0] + phi) ** 2))
+			if abs(x1 - x2 - np.pi) < xeps:
+				foutx.write("{:7f}\n".format(t))
+				poincaretimes.append(t)
+
+		#Progress
+		if int(t / dt) % int(float(T) / 50 / dt) == 0:
+			print int(t / dt) / int(float(T) / 100 / dt)
+
+		#Update history
+		x1hist.append(x1)
+		x2hist.append(x2)
+		x1hist.pop(0)
+		x2hist.pop(0)
+		t += dt
+	fout.close()
+elif not deterministic:
 	finpop = open(str(simulation) + "pop.out", 'r')
 	finxs = open(str(simulation) + "xs.out", 'r')
 	photonpops = []
@@ -133,8 +182,7 @@ else:
 	for line in finpop:
 		photonpops.append(line)
 	photonpops.pop() #Last line is a bit buggy usually
-	runinfo = photonpops.pop(0).split() #First line is the info section
-	t = 0
+	runinfo = photonpops.pop(0).split() #First line is the info sectioN
 	for line in finxs:
 		poincaretimes.append(float(line))
 	for i in range(len(photonpops)):
@@ -142,6 +190,20 @@ else:
 	n = int(float(runinfo[2]))
 	lambda0timesTd = float(runinfo[1])
 	T = float(runinfo[0])
+else:
+	transtime = 0.1
+	fin = open('detint.out','r')
+	finxs = open('detxs.out','r')
+	intensities = []
+	for line in fin:
+		intensities.append(float(line))
+	T = intensities.pop(0)
+	sampspersec = intensities.pop(0)
+	timegraph = [x / sampspersec for x in range(int((T - transtime) * sampspersec))]
+
+	poincaretimes = []
+	for line in finxs:
+		poincaretimes.append(float(line))
 
 print 'Now graphing...'
 
@@ -173,40 +235,46 @@ def movingwindowcalc(t):
 #Get histograms
 ####################################
 
-w = Td / 4
-binw = w / 20
-offset = int(transcount * Td / binw)
-binno = int(T / binw) - offset
-movingwindow = [0] * (binno)
-#print binno
+if not deterministic:
+	w = Td / 4
+	binw = w / 20
+	offset = int(transcount * Td / binw)
+	binno = int(T / binw) - offset
+	movingwindow = [0] * (binno)
+	#print binno
 
-lowcnt = 0 #binsearch(photonpops, transcount * Td * 0.99)
-highcnt = 0 #binsearch(photonpops, transcount * Td)
-for i in range(offset, offset + binno):
-	#Sliding count - linear, suggested by Joe
-	while lowcnt < len(photonpops) and photonpops[lowcnt] < i * binw:
-		lowcnt += 1
-	while highcnt < len(photonpops) and photonpops[highcnt] < i * binw + w:
-		highcnt += 1
-	movingwindow[i - offset] = highcnt - lowcnt
+	lowcnt = 0 #binsearch(photonpops, transcount * Td * 0.99)
+	highcnt = 0 #binsearch(photonpops, transcount * Td)
+	for i in range(offset, offset + binno):
+		#Sliding count - linear, suggested by Joe
+		while lowcnt < len(photonpops) and photonpops[lowcnt] < i * binw:
+			lowcnt += 1
+		while highcnt < len(photonpops) and photonpops[highcnt] < i * binw + w:
+			highcnt += 1
+		movingwindow[i - offset] = highcnt - lowcnt
 
-#print movingwindow
-#Toss out initial transient phase
-print 'Histogram complete!'
-timegraph = [x * binw for x in range(binno)]
+	#print movingwindow
+	#Toss out initial transient phase
+	print 'Histogram complete!'
+	timegraph = [x * binw for x in range(binno)]
 
 #Autocorrelation
 #############################
 if autocorr:
-	tinterval = 2 * offset #length of the section of the graph we will be shifting and comparing
-	mws = movingwindow[:tinterval] #mws = movingwindowslice
-	iterlen = offset / 20 #how far to shift along the graph
+	if not deterministic:
+		tinterval = 2 * offset #length of the section of the graph we will be shifting and comparing
+		mws = movingwindow[:tinterval] #mws = movingwindowslice
+		iterlen = offset / 20 #how far to shift along the graph
+	else:
+		tinterval = int(0.05 * sampspersec)
+		mws = intensities[:tinterval]
+		iterlen = int(0.02 * sampspersec)
+
 	mean = np.average(mws)
 	C = []
-
 	for shift in range (iterlen):
 		j = shift
-		ans = 0
+		ans = 0.
 		while j < tinterval:
 			ans += (mws[j-shift] - mean) * (mws[j] - mean)
 			j += 1
@@ -229,38 +297,49 @@ else:
 #Poincare section
 ################################
 if poincare:
-	if lambda0timesTd < 13:
-		pbinw = 1
-		maxp = 8
-	elif lambda0timesTd < 210:
-		pbinw = 1
-		maxp = 80
+	if not deterministic:
+		if lambda0timesTd < 13:
+			pbinw = 1
+			maxp = 8
+		elif lambda0timesTd < 210:
+			pbinw = 1
+			maxp = 80
+		else:
+			pbinw = 4
+			maxp = 800
+
+		psec = [[int(_) for _ in x] for x in np.zeros((maxp / pbinw, maxp / pbinw))] #(N_w(t), N_w(t - Td/4))
+		for ptime in poincaretimes:
+			if ptime >= transcount * Td + Td / 4:
+				nwp = movingwindowcalc(ptime) #cause we already chopped off a transcount
+				nwpt = movingwindowcalc(ptime - Td / 4)
+				if nwp >= maxp or nwpt >= maxp:
+					#Don't wanna deal with outliers
+					continue
+				psec[int(nwp / pbinw)][int(nwpt / pbinw)] += 1
+
+		print 'Poincare section done!'
+
+		#3D Diagram
+		bigplot = [[],[],[]]
+		for i in range(100,20000):
+			tt = transcount * Td + binw * i
+			bigplot[0].append(movingwindowcalc(tt))
+			bigplot[1].append(movingwindowcalc(tt - Td / 4))
+			bigplot[2].append(movingwindowcalc(tt - 2 * Td / 4))
+
+		print '3d attractor done!'
 	else:
-		pbinw = 4
-		maxp = 800
+		pbinw = 0.01
+		maxp = 1
+		psec = [[int(_) for _ in x] for x in np.zeros((maxp / pbinw, maxp / pbinw))] #(N_w(t), N_w(t - Td/4))
+		for ptime in poincaretimes:
+			if ptime > Td:
+				nwp = intensities[int((ptime - transtime) * sampspersec)]
+				nwpt = intensities[int((ptime - transtime - Td / 4) * sampspersec)]
+				psec[int(nwp / pbinw)][int(nwpt / pbinw)] += 1
 
-	psec = [[int(_) for _ in x] for x in np.zeros((maxp / pbinw, maxp / pbinw))] #(N_w(t), N_w(t - Td/4))
-	for ptime in poincaretimes:
-		if ptime >= transcount * Td + Td / 4:
-			nwp = movingwindowcalc(ptime) #cause we already chopped off a transcount
-			nwpt = movingwindowcalc(ptime - Td / 4)
-			if nwp >= maxp or nwpt >= maxp:
-				#Don't wanna deal with outliers
-				continue
-			psec[int(nwp / pbinw)][int(nwpt / pbinw)] += 1
-
-	print 'Poincare section done!'
-
-	#3D Diagram
-	bigplot = [[],[],[]]
-	for i in range(100,20000):
-		tt = transcount * Td + binw * i
-		bigplot[0].append(movingwindowcalc(tt))
-		bigplot[1].append(movingwindowcalc(tt - Td / 4))
-		bigplot[2].append(movingwindowcalc(tt - 2 * Td / 4))
-
-	print '3d attractor done!'
-
+		print 'Poincare section done!'
 #Variance
 #############################
 if variance:
@@ -285,21 +364,31 @@ if variance:
 ####################################
 
 plt.figure(1)
-plt.title('lambda0 * Td = ' + str(lambda0timesTd))
-plt.subplot(311)
-plt.xlim([0,T - transcount * Td])
-plt.plot(timegraph, movingwindow)
-plt.subplot(312)
-plt.hist(movingwindow, bins = range(0,900,3))
-plt.subplot(313)
-plt.plot(Cgraph)
+if deterministic:
+	plt.subplot(311)
+	plt.title('Deterministic')
+	plt.xlim([0,T - transtime])
+	plt.plot(timegraph, intensities)
+	plt.subplot(312)
+	plt.hist(intensities, bins = 20)
+	plt.subplot(313)
+	plt.plot(Cgraph)
+else:	
+	plt.subplot(311)
+	plt.title('lambda0 * Td = ' + str(lambda0timesTd))
+	plt.xlim([0,T - transcount * Td])
+	plt.plot(timegraph, movingwindow)
+	plt.subplot(312)
+	plt.hist(movingwindow)
+	plt.subplot(313)
+	plt.plot(Cgraph)
 
 if poincare:
 	plt.figure(2)
 	plt.pcolor(np.array(psec))
-	fig = plt.figure(3)
-	ax = fig.add_subplot(111, projection='3d')
-	ax.plot(bigplot[0],bigplot[1],bigplot[2])
+	#fig = plt.figure(3)
+	#ax = fig.add_subplot(111, projection='3d')
+	#ax.plot(bigplot[0],bigplot[1],bigplot[2])
 
 if variance:
 	plt.figure(4)
